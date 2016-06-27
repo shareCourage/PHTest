cb5f7469127fe36242aa33fc4fafd79aafdccdf1
haohua pan <panhaohua2014@yahoo.com>
2016-06-27 23:18:13 +0800

    Initial Commit

---
+++ PHTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHTest
+//
+//  Created by haohua pan on 16/6/27.
+//  Copyright © 2016年 haohua pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHTestTests/PHTestTests.m
@@ -0,0 +1,39 @@
+//
+//  PHTestTests.m
+//  PHTestTests
+//
+//  Created by haohua pan on 16/6/27.
+//  Copyright © 2016年 haohua pan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PHTestTests : XCTestCase
+
+@end
+
+@implementation PHTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
